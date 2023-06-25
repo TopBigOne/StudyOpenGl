7962223cd0c2e160ba5f37be4ceb0f71f7221649
guangya.zhou <chinagyzhou@gmail.com>
2023-06-25 17:50:25 +0800

    研究一下，，，缩放 三角形；

--- app/src/main/cpp/sample/TriangleSample.cpp
+++ app/src/main/cpp/sample/TriangleSample.cpp
@@ -26,23 +26,23 @@ void TriangleSample::Init() {
     }
 
     char vShaderStr[] =
-            "#version 300 es                          \n"
-            "layout(location = 2) in vec4 vPosition;  \n"
-            "void main()                              \n"
-            "{                                        \n"
-            "   gl_Position = vPosition;              \n"
-            "}                                        \n";
+                 "#version 300 es                          \n"
+                 "layout(location = 2) in vec4 vPosition;  \n"
+                 "void main()                              \n"
+                 "{                                        \n"
+                 "   gl_Position = vPosition;              \n"
+                 "}                                        \n";
 
     char fShaderStr[] =
-            "#version 300 es                              \n"
-            "precision mediump float;                     \n"
-            "out vec4 fragColor;                          \n"
-            "void main()                                  \n"
-            "{                                            \n"
-            "   fragColor = vec4 ( 1.0, 0.0, 0.0, 1.0 );  \n"
-            "}                                            \n";
+                 "#version 300 es                              \n"
+                 "precision mediump float;                     \n"
+                 "out vec4 fragColor;                          \n"
+                 "void main()                                  \n"
+                 "{                                            \n"
+                 "   fragColor = vec4 ( 1.0, 0.0, 0.0, 1.0 );  \n"
+                 "}                                            \n";
 
-    m_ProgramObj = GLUtils::CreateProgram(vShaderStr, fShaderStr, m_VertexShader,m_FragmentShader);
+    m_ProgramObj = GLUtils::CreateProgram(vShaderStr, fShaderStr, m_VertexShader, m_FragmentShader);
 
 
 }
@@ -54,29 +54,39 @@ void TriangleSample::Draw(int screenW, int screenH) {
             -0.5f, -0.5f, 0.0,
             0.5f, -0.5f, 0.0f
     };
-      GLfloat vVertices1[] = {
+
+    GLfloat vVertices1[] = {
             0.0f, 0.3f, -0.9f,
             -0.3f, -0.3f, -0.9f,
             0.3f, -0.3f, 1.0f
     };
 
+    glm::vec3 myVertices[] = {
+            glm::vec3(0.0f, 0.5f, 0.0),
+            glm::vec3(-0.5f, -0.5f, 0.0),
+            glm::vec3(0.5f, -0.5f, 0.0f)
+    };
+    glm::vec3 scale(2, 2, 0);
+    for (auto &myVerticse: myVertices) {
+        myVerticse *= scale;
+    }
 
-    if(m_ProgramObj==0){
+    if (m_ProgramObj == 0) {
         return;
     }
 
     int vPositionIndex = 2;
     glUseProgram(m_ProgramObj);
-    glVertexAttribPointer(vPositionIndex,3,GL_FLOAT,GL_FALSE,0,vVertices1);
+    glVertexAttribPointer(vPositionIndex, 3, GL_FLOAT, GL_FALSE, 0, myVertices);
     glEnableVertexAttribArray(vPositionIndex);
 
-    glDrawArrays(GL_TRIANGLES,0,3);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
     glUseProgram(GL_NONE);
 
 }
 
 void TriangleSample::Destroy() {
-    if(m_ProgramObj){
+    if (m_ProgramObj) {
         glDeleteProgram(m_ProgramObj);
         m_ProgramObj = GL_NONE;
     }
